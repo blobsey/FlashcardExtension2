f04f54be411bed5d87ec8ac3b1faf95564049291
blobsey <blobs03@gmail.com>
2024-09-14 20:05:49 -0800

    move back button to common.tsx

--- src/common/common.tsx
+++ src/common/common.tsx
@@ -164,3 +164,13 @@ export function renderMarkdown(text: string | null | undefined): React.ReactElem
         />
     )
 }
+
+/* Components */
+
+export const BackButton: React.FC<{ onClick: () => void }> = ({ onClick }) => (
+    <button className="blobsey-back-button absolute left-4 top-4" onClick={onClick}>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/>
+        </svg>
+    </button>
+);
--- src/content/Overlay.tsx
+++ src/content/Overlay.tsx
@@ -3,21 +3,13 @@ import FlashcardScreen from "./FlashcardScreen";
 import GradeScreen from "./GradeScreen";
 import usePersistentState from "../common/usePersistentState";
 import { Flashcard } from "../common/types";
-import { reviewFlashcard, editFlashcard, GRADES } from "../common/common";
+import { reviewFlashcard, editFlashcard, GRADES, BackButton } from "../common/common";
 import '../styles/tailwind.css';
 import ReviewScreen from "./ReviewScreen";
 import EditScreen from "./EditScreen";
 
 type Screen = 'flashcard' | 'grade' | 'review' | 'edit';
 
-// BackButton component defined within Overlay.tsx
-const BackButton: React.FC<{ onClick: () => void }> = ({ onClick }) => (
-    <button className="blobsey-back-button absolute left-4 top-4" onClick={onClick}>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-            <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/>
-        </svg>
-    </button>
-);
 
 const Overlay: React.FC = () => {
     // Global states
