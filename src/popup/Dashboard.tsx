5446c8812e28d8ac5757133065f0d3c09a42b73b
blobsey <blobs03@gmail.com>
2024-09-22 00:06:11 -0700

    wow popup menu so good

--- src/popup/Dashboard.tsx
+++ src/popup/Dashboard.tsx
@@ -1,19 +1,25 @@
 import React, { useState, useEffect } from 'react';
+import * as Checkbox from '@radix-ui/react-checkbox';
+import { CheckIcon, Pencil1Icon, TrashIcon, CheckCircledIcon, CrossCircledIcon } from '@radix-ui/react-icons';
 import usePersistentState from '../common/usePersistentState';
 import { useDebounce } from '../common/useDebounce';
 import { getUserData, updateUserData } from '../common/common';
-import { UserData } from '../common/types';
+import { UserData, BlockedSite } from '../common/types';
 import '../styles/popup-tailwind.css';
 
 const Dashboard: React.FC = () => {
     const [apiBaseUrl, _] = usePersistentState('apiBaseUrl', '');
     const [localUserData, setLocalUserData] = useState<Partial<UserData> | null>(null);
+    const [blockedSites, setBlockedSites] = useState<BlockedSite[]>([]);
+    const [editingSiteIndex, setEditingSiteIndex] = useState<number | null>(null);
+    const [editingUrl, setEditingUrl] = useState<string>('');
 
     useEffect(() => {
         const fetchUserData = async () => {
             try {
                 const userData = await getUserData();
                 setLocalUserData(userData);
+                setBlockedSites(userData.blocked_sites || []);
             } catch (error) {
                 console.error('Error fetching user data:', error);
             }
@@ -51,12 +57,46 @@ const Dashboard: React.FC = () => {
         }
     };
 
+    const handleSiteActiveChange = (index: number) => {
+        const updatedSites = [...blockedSites];
+        updatedSites[index].active = !updatedSites[index].active;
+        setBlockedSites(updatedSites);
+        updateUserData({ blocked_sites: updatedSites });
+    };
+
+    const handleEditSite = (index: number) => {
+        setEditingSiteIndex(index);
+        setEditingUrl(blockedSites[index].url);
+    };
+
+    const handleDeleteSite = (index: number) => {
+        const updatedSites = blockedSites.filter((_, i) => i !== index);
+        setBlockedSites(updatedSites);
+        updateUserData({ blocked_sites: updatedSites });
+    };
+
+    const handleSaveEdit = () => {
+        if (editingSiteIndex !== null) {
+            const updatedSites = [...blockedSites];
+            updatedSites[editingSiteIndex].url = editingUrl;
+            setBlockedSites(updatedSites);
+            updateUserData({ blocked_sites: updatedSites });
+            setEditingSiteIndex(null);
+        }
+    };
+
+    const handleCancelEdit = () => {
+        setEditingSiteIndex(null);
+        setEditingUrl('');
+    };
+
     return (
-        <div className="p-4">
-            <div>Logged in to: <code>{apiBaseUrl}</code></div>
+        <div className="p-4 w-full">
+            <div>Logged in to:</div>
+            <code>{apiBaseUrl}</code>
             
             <div className="mt-4">
-                <label htmlFor="maxNewCards" className="block mb-2">Max New Cards per Day:</label>
+                <label htmlFor="maxNewCards" className="block mb-2">Max new cards per day:</label>
                 <input
                     id="maxNewCards"
                     type="number"
@@ -68,9 +108,68 @@ const Dashboard: React.FC = () => {
             </div>
             
             <div className="mt-4">
+                <h4 className="font-semibold mb-2">Blocked Sites</h4>
+                <ul>
+                    {blockedSites.map((site, index) => (
+                        <li key={index} className="flex items-center mb-2">
+                            <Checkbox.Root
+                                className="flex h-5 w-5 items-center justify-center rounded border border-gray-300 bg-transparent hover:bg-gray-100/25 focus:outline-white transition-colors duration-200"
+                                checked={site.active}
+                                onCheckedChange={() => handleSiteActiveChange(index)}
+                            >
+                                <Checkbox.Indicator className="text-white">
+                                    <CheckIcon />
+                                </Checkbox.Indicator>
+                            </Checkbox.Root>
+                                <div className='flex-grow flex h-6 ml-2'>
+                                {editingSiteIndex === index ? 
+                                    <>
+                                        <input
+                                            type="text"
+                                            value={editingUrl}
+                                            onChange={(e) => setEditingUrl(e.target.value)}
+                                            className="flex-grow pl-2 p-1 border-none text-sm rounded"
+                                        />
+                                        <button
+                                            onClick={handleSaveEdit}
+                                            className="p-1 ml-2 text-green-500 hover:text-green-700"
+                                        >
+                                            <CheckCircledIcon />
+                                        </button>
+                                        <button
+                                            onClick={handleCancelEdit}
+                                            className="p-1 ml-2 text-red-500 hover:text-red-700"
+                                        >
+                                            <CrossCircledIcon />
+                                        </button>
+                                    </>
+                                    :
+                                    <>
+                                        <span className="ml-2 flex-grow">{site.url}</span>
+                                        <button
+                                            onClick={() => handleEditSite(index)}
+                                            className="p-1 ml-2 text-white/50 hover:text-white"
+                                        >
+                                            <Pencil1Icon />
+                                        </button>
+                                        <button
+                                            onClick={() => handleDeleteSite(index)}
+                                            className="p-1 ml-2 text-red-500 hover:text-red-700"
+                                        >
+                                            <TrashIcon />
+                                        </button>
+                                    </>
+                                }
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            
+            <div className="mt-4">
                 <button
                     onClick={() => browser.runtime.sendMessage({ action: 'logout' })}
-                    className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600"
+                    className="px-4 py-1 bg-red-500/25 text-white rounded hover:bg-red-500/50"
                 >
                     Logout
                 </button>
